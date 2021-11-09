68f9c1b55f104ff1171bcf154ff173cb6dc1b296
Mantecorso <luisjuradoquesada@gmail.com>
2021-11-09 13:07:39 +0100

    comentarios js

--- acordeon.js
+++ acordeon.js
@@ -1,8 +1,12 @@
 window.addEventListener('load', function() {
+
+    /* coge todos los elementos de la  clase acordeon */
     let elementosAcordeon = this.document.getElementsByClassName('acordeon');
 
     for (let i = 0; i<elementosAcordeon.length; i++) {
         elementosAcordeon[i].addEventListener('click', function() {
+            
+            /* cuando haces click en un elemento acordeon lo activas y al siguiente elemento cambias el display */
             this.classList.toggle('active');
             let panel = this.nextElementSibling;
             if (panel.style.display == "block") {
